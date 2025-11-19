0737a96282cf6fd1bb304891a168a5b0a5b93e61
Arya-Shidore <aryashidore2002@gmail.com>
2025-11-19 13:12:21 -0500

    api implemented and dockerfile added__to verify if dockerfile works

---
+++ database.py
@@ -0,0 +1,10 @@
+# database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL") 
+
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
